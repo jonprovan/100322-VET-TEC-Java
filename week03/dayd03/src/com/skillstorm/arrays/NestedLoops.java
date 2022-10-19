8fb14de864a4fbe4de08948c91b086bc8b761e14
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-10-19 10:58:24 -0400

    nested for loops

---
+++ week03/dayd03/src/com/skillstorm/arrays/NestedLoops.java
@@ -0,0 +1,14 @@
+package com.skillstorm.arrays;
+
+public class NestedLoops {
+	
+	public static void main(String[] args) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.println("i = " + i + ", j = " + j);
+				System.out.println("hello");
+			}
+		}
+	}
+
+}
