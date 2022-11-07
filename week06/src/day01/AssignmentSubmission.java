949be6d9a54f24d40157dc16c0a476efa9a178ac
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-11-07 15:49:43 -0500

    assignment assignment solution

--- week06/src/day01/AssignmentSubmission.java
+++ week06/src/day01/AssignmentSubmission.java
@@ -1,9 +1,84 @@
 package day01;
 
+import java.util.Objects;
+
 public class AssignmentSubmission {
-	String student; // name
-	int score; // 0 to 100
-	boolean cheated; // true means they cheated, false means they didn't
 	
+	private String student; // name
+	private int score; // 0 to 100
+	private boolean cheated; // true means they cheated, false means they didn't
+	
+	public AssignmentSubmission() {
+		this("Test Student", 0, false);
+	}
+	
+	public AssignmentSubmission(String student, int score) {
+		this(student, score, false);
+	}
+	
+	public AssignmentSubmission(String student) {
+		this(student, 0, false);
+	}
+	
+	public AssignmentSubmission(String student, int score, boolean cheated) {
+		setStudent(student);
+		setScore(score);
+		setCheated(cheated);
+	}
+
+	public String getStudent() {
+		return student;
+	}
+
+	public void setStudent(String student) {
+		if (student == null || student.isEmpty())
+			throw new IllegalArgumentException("Student must have a name");
+		this.student = student;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public void setScore(int score) {
+		if (score < 0)
+			throw new IllegalArgumentException("Score cannot be negative");
+		if (score > 100) 
+			throw new IllegalArgumentException("Score cannot be over 100");
+		this.score = score;
+	}
+
+	public boolean getCheated() {
+		return cheated;
+	}
+
+	public void setCheated(boolean cheated) {
+		this.cheated = cheated;
+	}
+
+	@Override
+	public String toString() {
+		String result =  student + " scored " + score;
+		if (cheated)
+			result += " but cheated";
+		return result;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cheated, score, student);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AssignmentSubmission other = (AssignmentSubmission) obj;
+		return cheated == other.cheated && score == other.score && Objects.equals(student, other.student);
+	}
 	
 }
--- week06/src/day01/MainClass.java
+++ week06/src/day01/MainClass.java
@@ -1,18 +1,85 @@
 package day01;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MainClass {
 	
 	public static void main(String[] args) {
-		List<AssignmentSubmission> submissions = null; // YOUR CODE HERE
+		List<AssignmentSubmission> submissions = 
+				Arrays.asList(new AssignmentSubmission("Sara", 80),
+							  new AssignmentSubmission("Cara", 70),
+							  new AssignmentSubmission("Lara", 100, true),
+							  new AssignmentSubmission("Tara", 0)); // YOUR CODE HERE
 		
-		// Convert to Stream
 		
 		// Problem 1: Find the highest score (excluding students that cheated)
 		
+		// STEP 0: STREAM
+		Stream<AssignmentSubmission> submissionStream = submissions.stream();
+		// STEP 1: FILTER
+		Stream<AssignmentSubmission> filtered = submissionStream.filter((s)->!s.getCheated());
+		// STEP 2: MAP to extract scores
+		Stream<Integer> scores = filtered.map((submission)->submission.getScore());
+		// STEP 3: REDUCE to find high score
+		int highScore = scores.reduce(0, (soFar, score) -> Math.max(soFar, score));
+		System.out.println("High score: " + highScore);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		// Problem 2: Find the low score (excluding students that didn't take the exam -- assume those are the only 0 scores)
 		
+		// STEP 0: STREAM
+		submissionStream = submissions.stream();
+		// STEP 1: FILTER
+		filtered = submissionStream.filter((student) -> student.getScore() > 0);
+		// STEP 2: MAP to extract scores
+		scores = filtered.map((student)->student.getScore());
+		// STEP 3: REDUCE to find low score
+		int lowScore = scores.reduce(100, (soFar, score) -> Math.min(soFar, score));
+		System.out.println("Low score: " + lowScore);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		// Problem 3: Set the scores of all the students who cheated to 0
+		
+		// STEP 0: STREAM
+		submissionStream = submissions.stream();
+		// STEP 2: MAP
+		Stream<AssignmentSubmission> updatedStream = submissionStream.map((submission) -> {
+			AssignmentSubmission updatedSubmission = new AssignmentSubmission();
+			updatedSubmission.setStudent(submission.getStudent());
+			updatedSubmission.setCheated(submission.getCheated());
+			if (submission.getCheated())
+				updatedSubmission.setScore(0);
+			else 
+				updatedSubmission.setScore(submission.getScore());
+			return updatedSubmission;
+		});
+		List<AssignmentSubmission> updatedScores = updatedStream.collect(Collectors.toList());
+		System.out.println(updatedScores);
 	}
 
 }
