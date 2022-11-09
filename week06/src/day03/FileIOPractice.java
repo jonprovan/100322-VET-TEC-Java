182fd0c332f307e9131afe2004eb19598c3c08f3
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2022-11-09 10:03:18 -0500

    writing to the same file

--- week06/src/day03/FileIOPractice.java
+++ week06/src/day03/FileIOPractice.java
@@ -3,6 +3,7 @@ package day03;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.LinkedList;
 
 public class FileIOPractice {
 	
@@ -21,16 +22,102 @@ public class FileIOPractice {
       */
 	
 	public static void main(String[] args) {
+		writeToFileOldWay();
+		
+		// TRY-WITH-RESOURCE -- Java handles closing the resource for me
+		//                   -- use anytime creating an instance of an AutoCloseable class (AutoCloseable is an interface)
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter("stuff.txt")) ) {
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// call methods with same file
+		LinkedList<String> lines = new LinkedList<>();
+		lines.add("never");
+		lines.add("gonna");
+		lines.add("give");
+		lines.add("you");
+		writeMultipleLinestToFile("./src/day03/myfiles/test.txt", lines);
+		writeStringToFile("./src/day03/myfiles/test.txt", "up");
+		
+	}
+	
+	public static void writeMultipleLinestToFile(String path, LinkedList<String> lines) {
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true)) ) { // append mode for FileWriter
+			// how do I get a line from my list?
+			for (String line : lines) {
+				// how do I write my line to my file?
+				writer.write(line);
+				writer.write("\n");
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void writeStringToFile(String path, String word) {
+		// step 1: create my writer
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true))) {
+			// step 2: write the word to the file
+			writer.write(word);
+			writer.write("\n"); // \n is a newline character
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void writeToFileOldWay() {
 		BufferedWriter writer = null; // delare it here and store null
+		// VERBOSE WAY
 		try {
 			//writer = new BufferedWriter(new FileWriter("output.txt", true)); // initialize it inside your try
-			writer = new BufferedWriter(new FileWriter("./src/day03/output.txt")); // default mode is WRITE -- writes over all the contents
+			writer = new BufferedWriter(new FileWriter("C:/Users/ehicks/Desktop/output2.txt")); // default mode is WRITE -- writes over all the contents
 			                                                           // to put in APPEND mode -- use a second parameter and set it to true
 			
 			writer.write("Hello world!"); // WHERE? the current folder our program is running in
 			                              // ./ means the current directory
 			                              // ../ means the parent directory
 			                              // ./src/day03/output.txt will put it under the diretory day03 under src
+			                              // "relative" vs "absolute" paths
+			
 			
 			// IMPORTANT
 			writer.close();
@@ -43,7 +130,5 @@ public class FileIOPractice {
 				e.printStackTrace();
 			}
 		}
-		
-		
 	}
 }
