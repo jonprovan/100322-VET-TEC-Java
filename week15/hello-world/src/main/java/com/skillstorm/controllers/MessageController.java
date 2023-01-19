d2352c6c694165d2752acd34d37d7fdf6848a03e
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2023-01-18 21:03:16 -0500

    wednesday rds material

--- week15/hello-world/src/main/java/com/skillstorm/controllers/MessageController.java
+++ week15/hello-world/src/main/java/com/skillstorm/controllers/MessageController.java
@@ -1,6 +1,7 @@
 package com.skillstorm.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import com.skillstorm.models.Message;
 import com.skillstorm.services.MessageService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class MessageController {
 	
 	@Autowired
