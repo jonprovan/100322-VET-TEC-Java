0225013d0b5168da78c8535e1078133ece3a28d3
Erica Hicks <48265629+ericahicks@users.noreply.github.com>
2023-01-20 16:31:21 -0500

    friday shop demo

--- week14/DAY04/shop-example/src/environments/environment.prod.ts
+++ week14/DAY04/shop-example/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiURL:"http://ec2-44-201-40-41.compute-1.amazonaws.com:8080"
 };
--- week14/day01/spring boot/spring-shop-example/src/main/java/com/skillstorm/practice/controllers/ShopController.java
+++ week14/day01/spring boot/spring-shop-example/src/main/java/com/skillstorm/practice/controllers/ShopController.java
@@ -1,12 +1,12 @@
 package com.skillstorm.practice.controllers;
 
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -29,6 +28,7 @@ import io.swagger.v3.oas.annotations.Operation;
 
 @Controller // This is better for controllers that return views (html pages)
 @RequestMapping("/shops")
+@CrossOrigin(origins = "*")
 @Tag(name = "Shop API", description = "A place to manage the shops that exist")
 public class ShopController {
 	
